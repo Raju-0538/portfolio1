167a19a91b850d5e2a82d9e1125d8a86fb63dccc
aravind000123 <aravind.gelam@sasi.ac.in>
2025-03-06 10:39:23 +0530

    Initial Commit

---
+++ components/Image.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import './image.css';
+import img2 from '../../src/transparent_background.png';
+
+function Image() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        // Trigger the animation once the component is mounted
+        setIsVisible(true);
+    }, []);
+
+    return (
+        <div className={`pro ${isVisible ? 'show' : ''}`}>
+            <img src={img2} alt="Animated" />
+        </div>
+    );
+}
+
+export default Image;
